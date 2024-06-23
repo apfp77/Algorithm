a5ac71457de6425e59a667bbb8862531d83d5b4f
apfp77 <apfp77@naver.com>
2024-06-23 10:49:23 +0900

    feat: 둘만의 암호

---
+++ programmers/level1/둘만의_암호.java
@@ -0,0 +1,25 @@
+public class 둘만의_암호 {
+    static class Solution {
+        public String solution(String s, String skip, int index) {
+            String answer = "";
+            for (char c: s.toCharArray()) {
+                char tmp = c;
+                int i = 0;
+                while (i < index){
+                    tmp =  tmp == 'z' ? 'a' : (char) (tmp + 1);
+                    if (skip.indexOf(tmp) > -1) {
+                        continue;
+                    }
+                    i++;
+                }
+                answer += tmp;
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("aukks","wbqd",5));
+    }
+}
